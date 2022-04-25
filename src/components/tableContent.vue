ac7d7dcbf7b1ad7807adc7a213c72483377181f8
huyanyou <1026855639@qq.com>
2022-04-25 13:49:40 +0800

    hongkong-paper basic

--- src/components/tableContent.vue
+++ src/components/tableContent.vue
@@ -1,13 +1,7 @@
 <template>
-  <div id="app">
-    <el-menu class="el-menu-demo" mode="horizontal"  background-color="#409EEF" >
-      <div id="header">
-        12131313
-        <span id="header-title">墨水瓶群控管理演示</span>
-      </div>
-    </el-menu>
-    <div class="interval"></div>
-    <!-- <el-table
+    <div>
+
+  <el-table
       :data="tableData"
       style="width: 100%">
       <el-table-column
@@ -61,14 +55,13 @@
         width="350">
         <template slot-scope="scope">
           <el-button type="text" :id="scope.index" @click="test(scope.$index)">编辑设备</el-button>&nbsp;&nbsp;&nbsp;&nbsp;
-          <el-button type="text">编辑设备</el-button>&nbsp;&nbsp;&nbsp;&nbsp;
-          <el-button type="text">编辑设备</el-button>
+          <el-button type="text">下发内容</el-button>&nbsp;&nbsp;&nbsp;&nbsp;
+          <el-button type="text">查看内容</el-button>
         </template>
       </el-table-column>
-    </el-table> -->
-    
+    </el-table>
 
-    <!-- <el-dialog title="编辑设备" :visible.sync="dialogFormVisible" >
+    <el-dialog title="编辑设备" :visible.sync="dialogFormVisible" center>
       <el-row>
         <el-col :span="12">
             <div>
@@ -103,40 +96,37 @@
               <el-form-item label="状态">
                   <el-input></el-input>
               </el-form-item>
+              <el-form-item>
+                <el-button @click="dialogFormVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+              </el-form-item>
             </el-form>
             </div>
         </el-col>
         <el-col :span="12">
           <div>
-
+            222222222222222
           </div>
         </el-col>
       </el-row>
 
 
-      <div slot="footer" class="dialog-footer">
+      <!-- <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">取 消</el-button>
         <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
-      </div>
-    </el-dialog> -->
+      </div> -->
+    </el-dialog>
+    </div>
+    
 
-    <table-content></table-content>
-    <el-row>
-      <el-button type="primary" id="btn">批量下发</el-button>
-    </el-row>
-
-    <div class="interval" id="bottom">1222222222222222</div>
-  </div>
+    
 </template>
 
 <script>
-import TableContent from './components/tableContent.vue'
-
 export default {
-  name: 'App',
-  data(){
-    return {
-      tableData: [{
+    data() {
+        return {
+            tableData: [{
             model: '7.5英寸黑白红',
             ip: '192.168.1.3',
             mac: '00:00:00:00:00:00',
@@ -217,86 +207,18 @@ export default {
         },
       formLabelWidth: '120px',
       labelPosition: 'right',
-    }
-    //===========================隐藏框=====================================
-  },
-  components: {
-    'table-content': TableContent,
-  },
-  methods: {
-    test(e){
+        }
+    },
+    methods:{
+      test(e){
       // console.log(e.target.id);
       console.log(e)
       this.dialogFormVisible = true;
+      }
     }
-  },
-  mounted() {
-    document.getElementsByClassName('el-table__body-wrapper')[0].style.overflowX = 'clip';
-  },
 }
 </script>
 
 <style>
-  * {
-    margin: 0;
-    padding: 0;
-  }
-  body{
-    position: relative;
-  }
-  
-  #header {
-    height: 60px;
-    padding-left: 2%;
-    line-height: 60px;
-  }
-
-  #header-title{
-    font-size: 20px;
-    font-weight: bold;
-    color: #fff;
-    margin-left: 20px;
-  }
-
-  .interval {
-    height: 30px;
-    background-color: #F5F7FA;
-  }
-
-  #bottom {
-    position: flex;
-    /* bottom: 999; */
-    z-index: 999;
-    top: 20px;
-
-  }
-
-  .el-table{
-    font-size: 1.2rem;
-  
-  }
-
-  #btn{
-    /* display:inline-block; */
-    float: right;
-    margin-right: 2%;
-    margin-top: 1%;
-  }
-
-  .el-form-item {
-  margin-right: 0 !important;
-  margin-left: 5%;
-  }
-  .el-form-item__label {
-    position: absolute;
-  }
-  .el-form-item__content {
-    width: 50%;
-    padding-left: 80px;
-  }
-  .el-select,
-  .el-input_inner {
-    width: 100%;
-  }
 
 </style>
